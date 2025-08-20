686bffc750a90b48262cc341142280aba11ea6f1
Josciline <u3312918@uni.canberra.edu.au>
2025-08-20 19:20:07 +1000

    Add files via upload

---
+++ Tutorial 4.py
@@ -0,0 +1,206 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+============== RESTART: C:\Users\U3312918\Downloads\coffee_shop.py =============
+Welcome to the Python Coffee Shop!
+
+============== RESTART: C:\Users\U3312918\Downloads\coffee_shop.py =============
+Welcome to the Python Coffee Shop!
+What is your name? Josciline
+Hello, Josciline! Let's order some coffee.
+
+============== RESTART: C:\Users\U3312918\Downloads\coffee_shop.py =============
+Welcome to the Python Coffee Shop!
+What is your name? Josciline
+Hello, Josciline! Let's order some coffee.
+Coffee: $3.5
+Latte: $4.0
+
+============== RESTART: C:\Users\U3312918\Downloads\coffee_shop.py =============
+Welcome to the Python Coffee Shop!
+What is your name? Josciline
+Hello, Josciline! Let's order some coffee.
+Coffee: $3.5
+Latte: $4.0
+Our menu: ['coffee', 'latte', 'espresso']
+
+============== RESTART: C:\Users\U3312918\Downloads\coffee_shop.py =============
+Welcome to the Python Coffee Shop!
+What is your name? Josciline
+Hello, Josciline! Let's order some coffee.
+Coffee: $3.5
+Latte: $4.0
+Our menu: ['coffee', 'latte', 'espresso']
+Welcome to the Python Coffee Shop!
+What is your name? Josciline
+Hello, Josciline! Let's order some coffee.
+Coffee: $3.5
+Latte: $4.0
+What would you like to order? (coffee/latte): mocha
+Sorry, we do not have that.
+How many cups would you like? 1
+Your total is: $0
+Thank you, Josciline! Please come again.
+
+============== RESTART: C:\Users\U3312918\Downloads\coffee_shop.py =============
+Welcome to the Python Coffee Shop!
+What is your name? Josciline
+Hello, Josciline! Let's order some coffee.
+Coffee: $3.5
+Latte: $4.0
+Our menu: ['coffee', 'latte', 'espresso']
+Welcome to the Python Coffee Shop!
+What is your name? Josciline
+Hello, Josciline! Let's order some coffee.
+Coffee: $3.5
+Latte: $4.0
+What would you like to order? (coffee/latte): coffee
+How many cups would you like? 1
+Your total is: $3.5
+Thank you, Josciline! Please come again.
+
+============== RESTART: C:\Users\U3312918\Downloads\coffee_shop.py =============
+Welcome to the Python Coffee Shop!
+What is your name? Josciline
+Hello, Josciline! Let's order some coffee.
+Coffee: $3.5
+Latte: $4.0
+Our menu: ['coffee', 'latte', 'mocha', 'espresso']
+Welcome to the Python Coffee Shop!
+What is your name? Josciline
+Hello, Josciline! Let's order some coffee.
+mocha: $5.0
+Coffee: $3.5
+Latte: $4.0
+What would you like to order? (coffee/latte/mocha): mocha
+How many cups would you like? 2
+You get a discount of $1.00!
+Your total is: $9.0
+Thank you, Josciline! Please come again.
+
+============== RESTART: C:\Users\U3312918\Downloads\coffee_shop.py =============
+Welcome to the Python Coffee Shop!
+What is your name? Ria
+Hello, Ria! Let's order some coffee.
+Coffee: $3.5
+Latte: $4.0
+Our menu: ['coffee', 'latte', 'mocha', 'espresso']
+Welcome to the Python Coffee Shop!
+What is your name? Ria
+Hello, Ria! Let's order some coffee.
+mocha: $5.0
+Coffee: $3.5
+Latte: $4.0
+What would you like to order? (coffee/latte/mocha): latte
+How many cups would you like? 2
+You get a discount of $1.00!
+are you a student?Yes
+Congratulation, you are eligible for 10% discount.
+Your total is: $6.3
+Thank you, Ria! Please come again.
+
+============== RESTART: C:\Users\U3312918\Downloads\coffee_shop.py =============
+Welcome to the Python Coffee Shop!
+What is your name? Raj
+Hello, Raj! Let's order some coffee.
+Coffee: $3.5
+Latte: $4.0
+Our menu: ['coffee', 'latte', 'mocha', 'espresso']
+Welcome to the Python Coffee Shop!
+What is your name? Raj
+Hello, Raj! Let's order some coffee.
+mocha: $5.0
+Coffee: $3.5
+Latte: $4.0
+What would you like to order? (coffee/latte/mocha): mocha
+How many cups would you like? 4
+You get a discount of $1.00!
+are you a student?no
+Your total is: $19.0
+Thank you, Raj! Please come again.
+468+751
+1219
+How are you?
+SyntaxError: invalid syntax
+print ("How are you?")
+How are you?
+print(8 / 3, 4 * 7, 9 + 13, 2 ** 5, 6 * (3 + 2))
+2.6666666666666665 28 22 32 30
+Size = 77
+x = 5
+y = 7
+ print(abs(x - y) - 10)
+ print(int(x ** 2) + 1.4)
+ print(round(y + 3.14159, 2))
+ 
+SyntaxError: multiple statements found while compiling a single statement
+x = 5
+y = 7
+ print(abs(x - y) - 10)
+ print(int(x ** 2) + 1.4)
+ print(round(y + 3.14159, 2))
+ 
+SyntaxError: multiple statements found while compiling a single statement
+KeyboardInterrupt
+x = 5
+y = 7
+print(abs(x - y) - 10)
+-8
+>>> print(int(x ** 2) + 1.4)
+26.4
+>>> print(round(y + 3.14159, 2))
+10.14
+>>> a = 31
+>>> b = 7
+>>> print(a // b, a % b)
+4 3
+>>> total minutes = 250
+SyntaxError: invalid syntax
+>>> total_minutes = 20
+>>> hours = total_mintes // 60
+Traceback (most recent call last):
+  File "<pyshell#17>", line 1, in <module>
+    hours = total_mintes // 60
+NameError: name 'total_mintes' is not defined. Did you mean: 'total_minutes'?
+>>> total_minutes = 250
+>>> hours = total_minutes // 60
+>>> minutes = total_minutes % 60
+>>> print ("Hours", hours)
+Hours 4
+>>> print ("Minutes", minutes)
+Minutes 10
+>>> str1 = "it is what it is"
+>>> print(str1.find("is"), str1.rfind("it"), str1[-9:-7])
+3 11 ha
+>>> print(str1[-9:])
+hat it is
+>>> a = 1
+>>> b = 1.5
+>>> 3 * a == 2 * b
+True
+>>> (a < b) or (b < a)
+True
+>>> not (a < b) or not (a < (b - a))
+True
+>>> ((a == b) or not (b < a)) and ((a < b) or (b == a + 1))
+True
+>>> 'Ab' == 'ab'
+False
+'Harry' < 'harry'
+True
+not (('B' == 'b') or ("Big" < "big"))
+False
+isinstance(32, float)
+False
+isinstance(32, int)
+True
+"yt" in "Python"
+True
+"knight".startswith('n')
+False
+True or False
+True
+True and False
+False
+not True
+False
